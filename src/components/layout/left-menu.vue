60e96d079e57db562756426600a501dc1f285483
Jose Maria Gayosso Ortega <jmgayosso@mightywizards.com>
2019-12-30 17:14:06 -0600

    Delete option of left menut

--- src/components/layout/left-menu.vue
+++ src/components/layout/left-menu.vue
@@ -30,51 +30,6 @@ export default {
         q-item-section(avatar)
           q-icon(name="app:chats")
         q-item-section {{ $t('menu.chats') }}
-      q-item(
-        v-if="isAuthenticated"
-        to="/profiles/contacts"
-        clickable
-        v-ripple
-      )
-        q-item-section(avatar)
-          q-icon(name="app:find-contacts")
-        q-item-section {{ $t('menu.contacts') }}
-      q-item(
-        to="/trails/treasuries"
-        clickable
-        v-ripple
-      )
-        q-item-section(avatar)
-          q-icon(name="app:groups")
-        q-item-section {{ $t('menu.trailsTreasuries') }}
-      q-item(
-        to="/trails/ballots"
-        clickable
-        v-ripple
-      )
-        q-item-section(avatar)
-          q-icon(name="app:decide")
-        q-item-section {{ $t('menu.trailsBallots') }}
-      q-item(
-        v-if="isAuthenticated"
-        to="/profiles/registerApp"
-        exact
-        clickable
-        v-ripple
-       )
-        q-item-section(avatar)
-          q-icon(name="fas fa-plus-square")
-        q-item-section {{ $t('menu.registerApp') }}
-      q-item(
-        v-if="isAuthenticated"
-        to="/profiles/appList"
-        exact
-        clickable
-        v-ripple
-       )
-        q-item-section(avatar)
-          q-icon(name="fas fa-list-alt")
-        q-item-section {{ $t('menu.appList') }}
 </template>
 
 <style lang="stylus">
