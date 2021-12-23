2a15c30f11c269bb590475116b5fd20e5aac5785
DaniPoot <danielpoot.uc2@gmail.com>
2021-12-23 12:51:31 -0600

    Fix label and values

--- src/pages/transactions/components/currency-transaction-modal.vue
+++ src/pages/transactions/components/currency-transaction-modal.vue
@@ -4,7 +4,7 @@ q-dialog(v-model="isEnable" persistent)
     q-card-section
       .text-bold.text-subtitle1 {{ $t('pages.transactions.currency_convertion') }}
     q-card-section.text-center
-      .text-h5 {{ debitCurrency }}
+      .text-h5 {{ debitCurrency.split(' ')[0] }}
       .text-caption {{ debitCurrency.split(' ')[1] }}
       q-separator.width-50
       .text-h5 {{ creditCurrency.split(' ')[0] }}
@@ -33,13 +33,13 @@ export default {
     creditCurrency () {
       const credit = this.components.filter(c => c.type === 'CREDIT')
       const currency = credit[0]?.currency || ''
-      const value = credit.map(c => c.quantity).reduce((previousValue, currentValue) => previousValue + currentValue, 0)
+      const value = credit.map(c => c.quantity).reduce((previousValue, currentValue) => Number(previousValue + currentValue), 0)
       return `${value} ${currency}`
     },
     debitCurrency () {
       const debit = this.components.filter(c => c.type === 'DEBIT')
       const currency = debit[0]?.currency || ''
-      const value = debit.map(c => c.quantity).reduce((previousValue, currentValue) => previousValue + currentValue, 0) || 0
+      const value = debit.map(c => c.quantity).reduce((previousValue, currentValue) => Number(previousValue + currentValue), 0)
       return `${value} ${currency}`
     }
   },
