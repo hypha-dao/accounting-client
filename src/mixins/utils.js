fea36d570c7ef4c93707b89c48c4c9c3639df298
DaniPoot <danielpoot.uc2@gmai.com>
2022-01-13 12:49:40 -0600

    Modified the regex to replace content

--- src/mixins/utils.js
+++ src/mixins/utils.js
@@ -44,7 +44,7 @@ export const utils = {
     },
     dateToString (_date) {
       if (!_date) return _date
-      var date = new Date(_date.replace(/ /g, 'T'))
+      var date = new Date(_date.replace(/T.*/g, ''))
       return (date.getDate() + 1) + ' ' + [
         'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul',
         'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
